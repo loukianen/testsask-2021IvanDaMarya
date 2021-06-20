089aae99ba4fd87572538960b9408309e7b2d6c9
Konstantin Lukyanenok <loukianen@gmail.com>
2021-06-20 11:08:50 +0300

    modify main.js

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-It is an application for calculation the salary.
+# Salary calculation (testtask)
+
+It is an application for calculation of the salary of a specific employee for a certain period.
+The customer is ![Web Studio "Иван да Марья"](https://www.ivandamari.ru/)
+
+## Use
 For use it at localhost you should clone this reposotory
 ```
 git clone https://github.com/loukianen/testtask-2021IvanDaMarya.git
--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 import $ from 'jquery';
 
+const colHeaders = [
+  'Дата',
+  'Начало рабочего дня',
+  'Конец рабочего дня',
+  'Кол-во генеральных уборок',
+  'Кол-во текущих уборок',
+  'Кол-во заездов',
+  'Сумма оплаты за день',
+];
+
 const makeRestTableHtml = (data) => {
   const dates = Object.keys(data).sort();
   const res = dates.map((date) => {
     const { start, end, sum, check_in, general, current, staff } = data[date];
-    const day = `<th scope="row"><a href="/days/?date=${date}&staff=${staff}" target="${date}">${date}</a></th>`;
-    const startDay = `<td>${start}</td>`;
-    const endDay = `<td>${end}</td>`;
-    const genCount = `<td>${general}</td>`;
-    const curCount = `<td>${current}</td>`;
-    const check_inCount = `<td>${check_in}</td>`;
-    const sumAmount = `<td>${sum}</td>`;
-    return `<tr>${day}${startDay}${endDay}${genCount}${curCount}${check_inCount}${sumAmount}</tr>`;
+    const dayColHtml = (
+      `<th scope="row">
+        <a href="/days/?date=${date}&staff=${staff}" target="${date}">${date}</a>
+      </th>`
+    );
+    const otherCols = [start, end, general, current, check_in, sum];
+    return `<tr>${dayColHtml}${otherCols.map((item) => `<td>${item}</td>`).join()}</tr>`;
   });
   return res;
 };
@@ -21,61 +30,44 @@ const calcCalary = (data) => {
   return dates.reduce((acc, day) => acc + data[day].sum, 0);
 };
 
-const renderTable = (data, node) => {
-  const tableElement = node;
-  // Считаем сумму за месяц
-  const salary = calcCalary(data);
-
-  // чистим таблицу на случай, если это не первый запрос
-  tableElement.innerHTML = null;
+const renderTable = (data, targetNode) => {
+  const tableElement = targetNode;
+  tableElement.html(null);
 
   // Массив, в который будем складывать строки таблицы
   const tableHtmlData = [];
-
   // заголовок
-  tableHtmlData.push('<thead><tr><th scope="col">Дата</th><th scope="col">Начало рабочего дня</th><th scope="col">Конец рабочего дня</th><th scope="col">Кол-во генеральных уборок</th><th scope="col">Кол-во текущих уборок</th><th scope="col">Кол-во заездов</th><th scope="col">Сумма оплаты за день</th></tr></thead>');
-
+  const colHeadersHtml = colHeaders.map((item) => `<th scope="col">${item}</th>`).join();
+  tableHtmlData.push(`<thead><tr>${colHeadersHtml}</tr></thead>`);
   // строки
   tableHtmlData.push(...makeRestTableHtml(data));
-  tableElement.innerHTML = `<table class="table table-striped text-center border-bottom">${tableHtmlData.join(' ')}</table>`;
+  // готовим таблицу
+  const table = $(`<table class="table table-striped text-center border-bottom"></table>`);
+  table.html(`${tableHtmlData.join(' ')}`);
 
-  // готовим элементы под итог за месяц и размещаем в DOM
-  const itogElement = document.createElement('div');
-  itogElement.setAttribute('class', 'd-flex justify-content-between p-3 font-weight-bold');
+  // готовим элементы с итогом за месяц
+  const itogElement = $('<div></div>')
+    .attr('class', 'd-flex justify-content-between p-3 font-weight-bold');
+  $('<div>Итоговая сумма</div>').appendTo(itogElement);
+  const salary = calcCalary(data);
+  $(`<div>${salary} рублей</div>`).appendTo(itogElement);
 
-  const itogTextElement = document.createElement('div');
-  itogTextElement.textContent = 'Итоговая сумма'
-
-  const itogSumElement = document.createElement('div');
-  itogSumElement.textContent = `${salary} рублей`;
-
-  itogElement.append(itogTextElement, itogSumElement);
-  tableElement.append(itogElement);
+  tableElement.append(table, itogElement);
 };
 
 const run = () => {
   $(document).ready(() => {
-    const mainElement = document.querySelector('main');
-
-    // создаем элемент для таблицы и помещаем в <main>
-    const tableElement = document.createElement('div');
-    tableElement.setAttribute('id', '#maintable');
-    mainElement.append(tableElement);
-
-    // навешиваем слушателя на форму
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
+    $('<div id="#maintable"></div>').appendTo('main');
+    $('form').submit((e) => {
       e.preventDefault();
-      // данные из формы
-      const userId = document.getElementById('name').value;
-      const period = document.getElementById('period').value;
+      const formData = new FormData(e.target);
+      const userId = formData.get('name');
+      const period = formData.get('period');
 
-      if (userId !== 'Выберите из списка' && period) {
-        // запрос на сервер
+      if (userId !== 'Выберите из списка' && period) { //To do
         $.get('/statistics', { userId, period })
-          .done((jsonData) => {
-            const data = JSON.parse(jsonData);
-            renderTable(data, tableElement);
+          .done((data) => {
+            renderTable(data, $('#maintable'));
           })
           .fail(() => console.log('Fail case'));
       }
