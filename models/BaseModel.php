55f08d113ce39c483529561401802788a0d5bbee
Konstantin Lukyanenok <loukianen@gmail.com>
2021-10-11 09:24:33 +0300

    modify BaseModel

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -8,11 +8,11 @@ class BaseModel
     public function getPrice($work)
     {
       $roomTypeQuery = 'SELECT `type` FROM rooms WHERE `id` = ' . $work['room'];
-      $roomType = $roomType = App::$db->query($roomTypeQuery)[0]['type'];
-      
-      $workPriceQuery = 'SELECT `price` FROM prices WHERE `room_type` = ' . $roomType . ' AND `work` = ' . $work['work'];
+      $workPriceQuery = 'SELECT `price` FROM prices
+        WHERE `room_type` = (' . $roomTypeQuery . ') AND `work` = ' . $work['work'];
+
       $price = App::$db->query($workPriceQuery)[0]['price'];
-      
+ 
       return $price;
     }
 }
